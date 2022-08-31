d66c6d63003c8ef32d1b31186630ac06b866f5a1
rickydamarsaputra <retrocode.rc@gmail.com>
2022-08-31 20:00:42 +0700

    5. selesai input cmd

---
+++ input_cmd.py
@@ -0,0 +1,8 @@
+# semua return dari input bertipe data string
+
+var_nama = input("masukan nama: ")
+
+print(f"nama anda adalah {var_nama}, bertipe {type(var_nama)}")
+
+var_umur = int(input("masukan umur: "))
+print(f"nama anda adalah {var_umur}, bertipe {type(var_umur)}")
