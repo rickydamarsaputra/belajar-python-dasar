ba104e185ce9b3d0a070b5614ce99631d02b1546
rickydamarsaputra <retrocode.rc@gmail.com>
2022-08-31 19:42:55 +0700

    4. selesai casting variable

---
+++ casting_tipe_data.py
@@ -0,0 +1,10 @@
+# casting adalah merubah suatu tipe data ke tipe data lain
+
+var_int = 10
+print("variable var_int bertipe: ", type(var_int))
+
+var_int = float(var_int)
+print("variable var_int bertipe: ", type(var_int))
+
+var_int = str(var_int)
+print("variable var_int bertipe: ", type(var_int))
