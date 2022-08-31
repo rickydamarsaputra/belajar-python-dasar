45d57e18361233d04446437d918c5292b2bc7ca3
rickydamarsaputra <retrocode.rc@gmail.com>
2022-08-31 19:12:37 +0700

    1. selesai hello world

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+# ini adalah comment satu baris
+
+"""ini adalah comment
+lebih dari satu baris"""
+
+# kita bisa mengcompile
+# python ke bytecode dengan menggunakan
+# "python -m py_compile hello_world.py"
+# hasil compile akan berada di folder __pycache__
+
+print("hello world")
