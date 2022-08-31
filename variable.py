38e0ce703c03395bf90bf53de46587ce5a359bdf
rickydamarsaputra <retrocode.rc@gmail.com>
2022-08-31 19:24:05 +0700

    2. selesai variable

---
+++ variable.py
@@ -0,0 +1,18 @@
+a = 10
+b = 5
+pi = 3.14
+nama_hewan = "sapi"
+
+print("nilai a adalah", a)
+print("nilai b adalah", b)
+print("nilai pi adalah", pi)
+print("nilai nama_hewan adalah", nama_hewan)
+
+print("######################")
+
+# replace nilai variable
+num1 = 100
+print("nilai num1 adalah", num1)
+
+num1 = 50
+print("nilai num1 adalah", num1)
